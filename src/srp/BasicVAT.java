ee7730bb385631f77d02f9c8dc0b7a793797f4fa
Mikel Laorden <laordenmikel@gmail.com>
2021-10-26 12:18:39 +0200

    Ortografia errore batzuk ebatzi srp paketeko klaseetan

--- src/srp/BasicVAT.java
+++ src/srp/BasicVAT.java
@@ -1,12 +1,10 @@
 package srp;
 
 public class BasicVAT implements VATCalculator {
-	public final float VATprecentage = (float) 0.16;
+	public final float VATpercentage = (float) 0.16;
 
 	@Override
 	public float calculateVAT(float billAmount) {
-		return (float) (billAmount * VATprecentage);
+		return (float) (billAmount * VATpercentage);
 	}
-
-	
 }
